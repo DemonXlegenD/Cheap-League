77344e23c7d551ffc68bd79198e623fefba98bd8
unknown <mbahri@gamingcampus.local>
2024-03-06 16:01:22 +0100

    Drone

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+using static UnityEngine.GraphicsBuffer;
 
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
     [SerializeField] private Transform ball;
+    [SerializeField] private GameObject arrowPointer;
+
     [SerializeField] private Vector3 offset;
     [SerializeField] private float translateSpeed;
     [SerializeField] private float rotationSpeed;
@@ -60,6 +65,11 @@ public class CameraFollow : MonoBehaviour
         HandleRotation();
     }
 
+    private Vector3 GetTargetPos()
+    {
+        return target.position + target.GetComponent<Rigidbody>().centerOfMass;
+    }
+
     public void HandleCamera(InputAction.CallbackContext context)
     {
         if (GetPressedKey(context.action))
@@ -75,31 +85,58 @@ public class CameraFollow : MonoBehaviour
         }
     }
 
+    private void TranslateCamera(Vector3 targetPosition)
+    {
+        RaycastHit hit;
+        Vector3 raycastOrigin = targetPosition;
+        if (Physics.Raycast(transform.position, targetPosition - transform.position, out hit, Vector3.Distance(transform.position, targetPosition)))
+        {
+            transform.position = Vector3.Lerp(transform.position, hit.point, translateSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
+        }
+    }
+
     private void HandleTranslation()
     {
         Vector3 targetPosition;
+        Vector3 targetDirection;
         if (cameraLocked)
         {
-            var targetDirection = target.position - ball.position + new Vector3(0, offset.y, 0);
-            targetPosition = target.position + new Vector3(0, offset.y / 2, 0) - (targetDirection.normalized * offset.z);
+            arrowPointer.SetActive(false);
+            targetDirection = GetTargetPos() - ball.position + new Vector3(0, 5, 0);
+            targetPosition = GetTargetPos() + new Vector3(0, offset.y, 0) - (targetDirection.normalized * offset.z * 2);
+            //transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
+            //TranslateCamera(targetPosition);
+            transform.position = Vector3.Lerp(GetTargetPos() + new Vector3(0, 1, 0), targetPosition, translateSpeed * Time.deltaTime);
+            //transform.LookAt(target.position);
         } else
         {
+            arrowPointer.SetActive(true);
+            targetDirection = ball.position - GetTargetPos();
+            arrowPointer.transform.position = GetTargetPos() + (targetDirection.normalized * 2);
+            arrowPointer.transform.LookAt(ball.position);
+            arrowPointer.transform.Rotate(90, 0, 0);
+
             targetPosition = target.TransformPoint(offset);
+            TranslateCamera(targetPosition);
+            //transform.position = Vector3.Lerp(transform.position + new Vector3(0, 2, 0), targetPosition, translateSpeed * Time.deltaTime);
         }
 
         //var finalPos = Physics.Raycast(transform.position, targetPosition.normalized, targetPosition.magnitude);
 
-        transform.position = Vector3.Lerp(transform.position, targetPosition, translateSpeed * Time.deltaTime);
     }
     private void HandleRotation()
     {
-        var direction = target.position - transform.position;
+        var direction = GetTargetPos() - transform.position;
         if (cameraLocked)
         {
             direction = ball.position - transform.position;
         }
 
-        var rotation = Quaternion.LookRotation(direction, Vector3.up);
+        var rotation = Quaternion.LookRotation(direction);
 
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
     }
